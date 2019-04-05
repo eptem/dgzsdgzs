c02bcca3266ad0c694d0b2191343a3498d383efa
Eptem <mropp1@mail.ru>
2019-04-05 17:47:08 +0500

    Initial Commit

---
+++ dgzsdgzs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dgzsdgzs
+//
+//  Created by Артем Жорницкий on 05/04/2019.
+//  Copyright © 2019 Артем Жорницкий. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
